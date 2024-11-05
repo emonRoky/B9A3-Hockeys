7da64060201c741db9f8523f139c8738ab128f54
Roky <apple@Rokys-MacBook-Air.local>
2024-11-06 00:27:19 +0900

    added nav, banner, slider

---
+++ tailwind.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          "primary-color": '#FF4240',
+          "gradient-end": '#F00',
+          "color-border": '#13131833',
+        }
+      }
+    }
+  }
