2993c29093aba08417d70cd34b72d139804e2279
Roky <apple@Rokys-MacBook-Air.local>
2024-11-06 20:25:05 +0900

    added progress section

--- tailwind.js
+++ tailwind.js
@@ -6,6 +6,9 @@ tailwind.config = {
           "primary-color": '#FF4240',
           "gradient-end": '#F00',
           "color-border": '#13131833',
+         "customGreen": '#49D293',
+         "customOrange": '#FFB546',
+         "customBlue": '#4C8DF1',
         }
       }
     }
